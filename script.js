90e608062f6de8548bf374b239033314a02e49e3
EsaRizvi22 <honeyrizvi701@gmail.com>
2024-10-08 16:30:48 +0300

    Activity 1

---
+++ script.js
@@ -0,0 +1,4 @@
+//this is for paragraph 1
+document.getElementById("p1").innerHTML="welcome to Javascript";
+//this is for paragraph 2
+document.getElementById("p2").innerHTML = "This is our first Javascript activity";
